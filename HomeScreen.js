09a44e997f05348df9b9fb1dacc305bf7237d591
alvezbabi <130004378+alvezbabi@users.noreply.github.com>
2024-11-14 15:55:41 -0300

    Adiciona tela HomeScreen.js

---
+++ HomeScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Menu Principal</Text>
+      <Button title="Cadastrar Serviço" onPress={() => navigation.navigate('CadastroServico')} />
+      <Button title="Cadastrar Cliente" onPress={() => navigation.navigate('CadastroCliente')} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+});
+
+export default HomeScreen;
