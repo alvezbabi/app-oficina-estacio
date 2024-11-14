12e87b3d9296203ca688099ad4430bb55a89755a
alvezbabi <130004378+alvezbabi@users.noreply.github.com>
2024-11-14 16:03:09 -0300

    Atualização das telas no APP

--- App.js
+++ App.js
@@ -5,6 +5,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './HomeScreen';
 import CadastroServico from './CadastroServico';
 import CadastroCliente from './CadastroCliente';
+import CadastroVeiculo from './CadastroVeiculo';
+import CadastroOrcamento from './CadastroOrcamento';
+import ConsultaInformacao from './ConsultaInformacao';
 
 const Stack = createStackNavigator();
 
@@ -15,6 +18,9 @@ export default function App() {
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Menu Principal' }} />
         <Stack.Screen name="CadastroServico" component={CadastroServico} options={{ title: 'Cadastro de Serviço' }} />
         <Stack.Screen name="CadastroCliente" component={CadastroCliente} options={{ title: 'Cadastro de Cliente' }} />
+        <Stack.Screen name="CadastroVeiculo" component={CadastroVeiculo} options={{ title: 'Cadastro de Veículo' }} />
+        <Stack.Screen name="CadastroOrcamento" component={CadastroOrcamento} options={{ title: 'Cadastro de Orçamento' }} />
+        <Stack.Screen name="ConsultaInformacao" component={ConsultaInformacao} options={{ title: 'Consulta de Informação' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
